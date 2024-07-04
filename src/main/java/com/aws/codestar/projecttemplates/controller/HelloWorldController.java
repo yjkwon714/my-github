8f762818991d16aa1c32eafc16dcec299b6fa145
yjkwon714 <choco714@gmail.com>
2024-07-04 11:48:55 +0900

    Add logger

--- src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     private static final String MESSAGE_FORMAT = "Hello %s!";
+    private static final String logger = "Let's trigger #2!";
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity helloWorldGet(@RequestParam(value = "name", defaultValue = "World") String name) {
